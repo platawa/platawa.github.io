3d770445ed47ce1fa275bc156489b32918da2178
platawa <paridhi.latawa@gmail.com>
2026-02-22 19:09:10 +0000

    Deploying to gh-pages from @ platawa/platawa.github.io@245cc976ed58dcdab3c2b76657ced85dae958d37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,6 +492,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/paridhilatawa", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/parilatawa", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
